a5088d05dc051604e28d66f0f6f7cd5112c362d8
dflk <904885508@qq.com>
2021-12-15 00:04:50 +0800

    Next 全局配置

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import 'styles/globals.css'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>我的博客 - sun</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/posts/first.js
+++ pages/posts/first.js
@@ -1,7 +1,6 @@
 import React, {useCallback} from "react";
 import Link from 'next/link'
 
-
 console.log('zhixing')
 export default function First() {
     //这种写法可以防止函数创建多次
