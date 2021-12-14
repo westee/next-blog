ba13cb7afb8d9738b146a45a8fe7be958ef27513
dflk <904885508@qq.com>
2021-12-14 23:06:46 +0800

    同构代码

--- pages/posts/first.js
+++ pages/posts/first.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useCallback} from "react";
 import Link from 'next/link'
-export default function first() {
+
+
+console.log('zhixing')
+export default function First() {
+    //这种写法可以防止函数创建多次
+    const clickMe = useCallback(()=>{
+        console.log('click')
+    }, [])
     return (
         <div>
+            <button onClick={clickMe}> click me</button>
             <Link href="/"><a >到首页</a></Link>
         </div>
     )
